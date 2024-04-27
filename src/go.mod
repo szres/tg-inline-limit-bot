3c5a5a5cb2b38a6dc0528ff90eb6a31e89a16b3d
xianii <xianyi.xia@outlook.com>
2024-04-27 22:06:51 +0800

    first work version

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,11 @@
 module app
 
 go 1.21
+
+require (
+	github.com/Nigh/kuma-push v0.1.0
+	github.com/nanobox-io/golang-scribble v0.0.0-20190309225732-aa3e7c118975
+	gopkg.in/telebot.v3 v3.2.1
+)
+
+require github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25 // indirect
--- src/main.go
+++ src/main.go
@@ -2,31 +2,159 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"log"
 	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
 	"time"
+
+	kuma "github.com/Nigh/kuma-push"
+	scribble "github.com/nanobox-io/golang-scribble"
+	tele "gopkg.in/telebot.v3"
 )
 
-var buildTime string
-var startTime string
-var envName string
+type testenv struct {
+	Token   string `json:"token"`
+	AdminID string `json:"adminid"`
+	KumaURL string `json:"kumaurl"`
+}
 
-func index(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "<html><head><title>HELLO</title><meta http-equiv=\"refresh\" content=\"1\"></head>\n")
+var testEnv testenv
 
-	fmt.Fprintf(w, "<body><h1>hello</h1>\n")
-	fmt.Fprintf(w, "<table>\n")
-	fmt.Fprintf(w, "<tr><td>env name</td><td>%s</td></tr>\n", envName)
-	fmt.Fprintf(w, "<tr><td>build time</td><td>%s</td></tr>\n", buildTime)
-	fmt.Fprintf(w, "<tr><td>start time</td><td>%s</td></tr>\n", startTime)
-	fmt.Fprintf(w, "<tr><td>current time</td><td>%s</td></tr>\n", time.Now().Format("20060201-150405"))
-	fmt.Fprintf(w, "</table></body>\n</html>")
+var (
+	gTimeFormat      string = "2006-01-02 15:04"
+	gRootID          int64
+	gKumaPushURL     string
+	gToken           string
+	gCooldownMinutes int = 240
+)
+
+type InlineCooldown struct {
+	// Cooldown time left in minutes
+	Cooldown int
+	// Count of inline messages
+	Count int
+}
+
+var db *scribble.Driver
+var inlineCD map[string]InlineCooldown
+
+func burnoutCheck(c tele.Context) error {
+	combinedID := strconv.FormatInt(c.Chat().ID, 10) + ":" + strconv.FormatInt(c.Sender().ID, 10)
+	value, ok := inlineCD[combinedID]
+	if !ok {
+		value = InlineCooldown{gCooldownMinutes, 1}
+		inlineCD[combinedID] = value
+	} else {
+		if value.Count >= 4 {
+			c.Delete()
+			return c.Send(fmt.Sprintf("[%s](tg://user?id=%d) %s", escape(fullName(c.Sender())), c.Sender().ID, escape("your inline message burned out! It may take significant time for reset.")), tele.ModeMarkdownV2)
+		} else {
+			value.Count++
+			inlineCD[combinedID] = value
+		}
+	}
+	return nil
+}
+
+func inlineCooldownRoutine() {
+	interval := time.NewTicker(1 * time.Minute)
+	defer interval.Stop()
+	for range interval.C {
+		keysToRemove := make([]string, 0)
+		for k, v := range inlineCD {
+			v.Cooldown--
+			inlineCD[k] = v
+			if v.Cooldown <= 0 {
+				keysToRemove = append(keysToRemove, k)
+			}
+		}
+		for _, k := range keysToRemove {
+			delete(inlineCD, k)
+		}
+	}
+}
+
+func init() {
+	db, _ = scribble.New("../db", nil)
+	db.Read("test", "env", &testEnv)
+	inlineCD = make(map[string]InlineCooldown)
+	db.Read("data", "inline", &inlineCD)
+	go inlineCooldownRoutine()
+	if testEnv.Token != "" {
+		gToken = testEnv.Token
+		gRootID, _ = strconv.ParseInt(testEnv.AdminID, 10, 64)
+		gKumaPushURL = testEnv.KumaURL
+	} else {
+		gToken = os.Getenv("BOT_TOKEN")
+		gRootID, _ = strconv.ParseInt(os.Getenv("BOT_ADMIN_ID"), 10, 64)
+		gKumaPushURL = os.Getenv("KUMA_PUSH_URL")
+	}
+	fmt.Printf("gToken:%s\ngRootID:%d\ngKumaPushURL:%s\n", gToken, gRootID, gKumaPushURL)
+	k := kuma.New(gKumaPushURL)
+	k.Start()
+}
+
+func escape(s string) string {
+	var escapeList string = `_*[]()~` + "`" + `>#+-=|{}.!`
+	prefix := ""
+	result := ""
+	for _, c := range s {
+		char := string(c)
+		if strings.ContainsRune(escapeList, rune(c)) && prefix != `\` {
+			result += `\` + char
+		} else {
+			result += char
+		}
+		prefix = char
+	}
+	return result
+}
+func fullName(u *tele.User) string {
+	if u.FirstName == "" || u.LastName == "" {
+		return u.FirstName + u.LastName
+	}
+	return u.FirstName + " " + u.LastName
 }
 
 func main() {
-	envName = os.Getenv("NAME")
+	var b *tele.Bot
 
-	startTime = time.Now().Format("20060201-150405")
-	http.HandleFunc("/", index)
-	http.ListenAndServe(":11180", nil)
+	pref := tele.Settings{
+		Token:  gToken,
+		Poller: &tele.LongPoller{Timeout: 5 * time.Second},
+	}
+	var err error
+	b, err = tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle(tele.OnText, func(c tele.Context) error {
+		if c.Message().Via != nil {
+			return burnoutCheck(c)
+		}
+		return nil
+	})
+	b.Handle(tele.OnPhoto, func(c tele.Context) error {
+		if c.Message().Via != nil {
+			return burnoutCheck(c)
+		}
+		return nil
+	})
+
+	go b.Start()
+
+	fmt.Println("bot online!")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, os.Interrupt, syscall.SIGTERM)
+	<-sc
+	b.Stop()
+	fmt.Println("before shutdown, backup data")
+	db.Write("data", "inline", inlineCD)
+	<-time.After(time.Second * 1)
+	fmt.Println("bot offline!")
 }
